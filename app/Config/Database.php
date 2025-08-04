722533d1ca9c7d7f330809c1ecedb646c7c8e858
kladriva <kladrivas@gmail.com>
2025-08-04 21:26:24 +0100

    connection  de la bd

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => '84.247.187.217',
         'username'     => 'root',
         'password'     => 'Jesus',
-        'database'     => '',
+        'database'     => 'kladrivaacademie',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
